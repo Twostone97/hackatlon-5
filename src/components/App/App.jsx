06546e23d9f63cadf62776d7db9f64e4f991aa30
David Řehoř <rehord@email.cz>
2019-07-05 15:12:52 +0200

    search fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,10 +27,10 @@ const App = () => {
     Pardubice: "PED"
   };
 
-  const getFlight = () => {
+  const getFlight = (fromCode, toCode) => {
     setLoading(true);
     fetch(
-      `https://api.skypicker.com/flights?fly_from=${from}&fly_to=${to}&date_from=05/07/2019&date_to=06/08/2019&direct_flights=${directFlight}`
+      `https://api.skypicker.com/flights?fly_from=${fromCode}&fly_to=${toCode}&date_from=05/07/2019&date_to=06/08/2019&direct_flights=${directFlight}`
     )
       .then(resp => resp.json())
       .then(({ data }) => {
@@ -48,9 +48,9 @@ const App = () => {
       .catch(console.log);
   };
 
-  useEffect(() => {
-    getFlight();
-  }, []);
+  // useEffect(() => {
+  //   getFlight();
+  // }, []);
 
   const handleFrom = e => {
     setFrom(fromList[e.target.value]);
@@ -90,27 +90,24 @@ const App = () => {
     setTextInputTo(e.target.value);
     // console.log(e.target.value);
   };
-  const getCityCode = () => {
+  const getCityCode = async () => {
+    let to, from;
     fetch(`https://api.skypicker.com/locations?term=${textInputFrom}&limit=1`)
       .then(resp => resp.json())
       .then(cityCodeFrom => cityCodeFrom.locations[0].code)
       .then(data => {
         console.log("input From");
-        setFrom(data);
+        from = data;
+        fetch(`https://api.skypicker.com/locations?term=${textInputTo}&limit=1`)
+          .then(resp => resp.json())
+          .then(cityCodeTo => cityCodeTo.locations[0].code)
+          .then(data => {
+            console.log("input to");
+            to = data;
+            getFlight(from, to);
+          });
       });
-
-    fetch(`https://api.skypicker.com/locations?term=${textInputTo}&limit=1`)
-      .then(resp => resp.json())
-      .then(cityCodeTo => cityCodeTo.locations[0].code)
-      .then(data => {
-        console.log("input to");
-        setTo(data);
-      });
-    console.log("getting fligh");
   };
-  useEffect(() => {
-    getFlight();
-  }, [from, to]);
 
   return (
     <>
